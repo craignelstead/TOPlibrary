63e68a307eaa06a6fdef3fb02415642e1e00b53a
craignelstead <ctnelst92@gmail.com>
2023-09-06 12:06:25 -0600

    Created bookshelf html and css layout with dummy books

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -8,15 +10,13 @@ function Book(title, author, pages, read) {
     }
 }
 
-const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-const book2 = new Book('The Tall One', 'J.K. Jokin', 590, true);
+// const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+// const book2 = new Book('The Tall One', 'J.K. Jokin', 590, true);
 
-console.log(book1.info());
-console.log(book2.info());
+// Book.prototype.sayHello = function() {
+//     console.log('Hello, I am a book');
+// }
 
-Book.prototype.sayHello = function() {
-    console.log('Hello, I am a book');
+function addBookToLibrary() {
+    
 }
-
-book1.sayHello();
-book2.sayHello();
