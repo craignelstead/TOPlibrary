ac97ee1e74ef9a865355494f2bae17327159ec5c
craignelstead <ctnelst92@gmail.com>
2023-09-08 15:03:32 -0600

    Updated displayBook method so all books in array are displayed on shelf

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
+//Get page elements: shelves, books, buttons, form
 const shelf1 = document.getElementById('topshelf');
 const shelf2 = document.getElementById('midshelf');
 const shelf3 = document.getElementById('bottomshelf');
 
+const bookAddBtn = document.getElementById('bookshelfbtn');
+
+const booksOnShelfNL = document.getElementsByClassName('book');
+const booksOnShelfArr = Array.from(booksOnShelfNL);
+
 const myLibrary = [];
 
+//Constructor for book
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return `${title} by ${author}, ${pages} pages, have ${read ? '' : 'not '}read`;
-    }
+
+    myLibrary.push(this);
 }
 
-// const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-// const book2 = new Book('The Tall One', 'J.K. Jokin', 590, true);
+let bookCount = booksOnShelfArr.length;
+
+console.log(bookCount);
+
+const book1 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, true);
+const book2 = new Book('The Very Hungry Caterpillar', 'Eric Carle', 32, true);
+const book3 = new Book('1984', 'George Orwell', 328, true);
 
 // Book.prototype.sayHello = function() {
 //     console.log('Hello, I am a book');
@@ -23,7 +34,30 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary() {
     /*
+    Validate input
     Take form info and create new object with it
-    Add new book to DOM
+    Add new book to DOM before last child
     */
 }
+
+Book.prototype.displayBook = function() {
+    for (let i = 0; i < bookCount; i++) {
+        const bookDiv = document.createElement('div');
+        bookDiv.classList.add('book');
+
+        const bookTitle = document.createElement('p');
+        bookTitle.classList.add('title');
+        bookTitle.textContent = `${this.title}`;
+        bookDiv.appendChild(bookTitle);
+
+        const bookAuthor = document.createElement('p')
+        bookAuthor.classList.add('author');
+        bookAuthor.textContent = `${this.author}`;
+        bookDiv.appendChild(bookAuthor);
+
+        //shelf1.appendChild(bookDiv);
+        shelf1.insertBefore(bookDiv, bookAddBtn)
+    }
+}
+
+myLibrary.forEach(function(book) {book.displayBook()});
